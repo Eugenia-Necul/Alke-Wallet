883cf90261bdb6534e1c3fa8fec90802a2f1caf1
Eugenia Necul <francisca.neculm@gmail.com>
2026-02-13 23:22:20 -0300

    se trabaja en validación de Login desde JavaScript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,25 @@
-const saldoDisponible = document.getElementById(saldoDisponible);
+
+
+const login_pantalla = document.getElementById("iniciaSesion");
+
+function validarLogin(event) {
+
+    event.preventDefault();
+
+    const formulario = event.target;
+
+    if ((formulario.rutUsuario.value === "12345678-9" && formulario.contrasena.value === "Prueba123")) {
+        location.href = "menu.html";
+    }
+    else {
+        alert("¡Rut o contraseña incorrectos!")
+    }
+} 
+
+if (login_pantalla) {
+    const formularioLogin = document.getElementById("loginForm");
+
+    formularioLogin.addEventListener('submit', validarLogin);
+
+}
+
